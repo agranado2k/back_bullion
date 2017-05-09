572afaca9bb3b97e4190297a30a8da5cc37f4d63
Arthur Granado <agranado@pluga.co>
2017-05-09 13:53:53 -0300

    Create the Student and Income entities

--- lib/back_bullion.rb
+++ lib/back_bullion.rb
@@ -1,3 +1,39 @@
 module BackBullion
+  class Student
+    attr_accessor :age, :job_role, :tuition_fee, :maintenance_loan, :exp_level
 
+    def initialize(args)
+      @age = args[:age]
+      @job_role = args[:job_role]
+      @tuition_fee = args[:tuition_fee]
+      @maintenance_loan = args[:maintenance_loan]
+      @exp_level = args[:exp_level]
+    end
+  end
+
+  class Income
+    EXP_LEVEL_TO_SALARY = {
+      '1st yr trainee' =>  38000,
+      '2nd yr trainee' =>  40000,
+      'NQ' =>  63000,
+      '1 yr pqe' =>  70000,
+      '2 yr pqe' =>  75000,
+      '3 yr pqe' =>  82000,
+      '4 yr pqe' =>  88000,
+      '5 yr pqe' =>  93000,
+      '6 yr pqe' =>  98000,
+      '7 yr pqe' =>  101000,
+      '8 yr pqe' =>  103000,
+      'junior partner' =>  180000,
+      'Equity partner' =>  260000
+    }
+
+    def initialize(student)
+      @student = student
+    end
+
+    def value
+      EXP_LEVEL_TO_SALARY[@student.exp_level]
+    end
+  end
 end
--- test/back_bullion_test.rb
+++ test/back_bullion_test.rb
@@ -2,7 +2,37 @@ require 'test_helper'
 
 class BackBullionTest < Minitest::Test
 
-  def test_fake
-    assert_equal true, true
+  def test_student
+    student_params = {age: 35, job_role: 'laywer', tuition_fee: 9000, maintenance_loan: 3500, exp_level: '1st yr trainee'}
+    
+    student = BackBullion::Student.new(student_params)
+
+    assert_equal student.age, 35
+    assert_equal student.job_role, 'laywer'
+    assert_equal student.tuition_fee, 9000
+    assert_equal student.maintenance_loan, 3500
+    assert_equal student.exp_level, '1st yr trainee'
+  end
+
+  def test_income_for_1st_yr_trainee
+    student_params = {age: 35, job_role: 'laywer', tuition_fee: 9000, maintenance_loan: 3500, exp_level: '1st yr trainee'}
+    student = BackBullion::Student.new(student_params)
+
+    income = BackBullion::Income.new(student)
+
+    assert_equal income.value, 38000
+  end
+
+  def test_income_for_4_yr_pqe
+    student_params = {age: 35, job_role: 'laywer', tuition_fee: 9000, maintenance_loan: 3500, exp_level: '4 yr pqe'}
+    student = BackBullion::Student.new(student_params)
+
+    income = BackBullion::Income.new(student)
+
+    assert_equal income.value, 88000
+  end
+
+  def test_repayment
+
   end
 end
