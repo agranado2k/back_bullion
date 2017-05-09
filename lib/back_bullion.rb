05f925a0bb105873162b4a27652c07fb9d158458
Arthur Granado <agranado@pluga.co>
2017-05-09 14:52:30 -0300

    Refactory: Create DBInterface and inject in Income and Repayment

--- lib/back_bullion.rb
+++ lib/back_bullion.rb
@@ -11,29 +11,76 @@ module BackBullion
     end
   end
 
-  class Income
-    EXP_LEVEL_TO_SALARY = {
-      '1st yr trainee' =>  38000,
-      '2nd yr trainee' =>  40000,
-      'NQ' =>  63000,
-      '1 yr pqe' =>  70000,
-      '2 yr pqe' =>  75000,
-      '3 yr pqe' =>  82000,
-      '4 yr pqe' =>  88000,
-      '5 yr pqe' =>  93000,
-      '6 yr pqe' =>  98000,
-      '7 yr pqe' =>  101000,
-      '8 yr pqe' =>  103000,
-      'junior partner' =>  180000,
-      'Equity partner' =>  260000
+  class DBInterface
+    JOB_ROLES = {
+        'laywer' => {
+        'student' => 0,
+        '1st yr trainee' =>  38000,
+        '2nd yr trainee' =>  40000,
+        'NQ' =>  63000,
+        '1 yr pqe' =>  70000,
+        '2 yr pqe' =>  75000,
+        '3 yr pqe' =>  82000,
+        '4 yr pqe' =>  88000,
+        '5 yr pqe' =>  93000,
+        '6 yr pqe' =>  98000,
+        '7 yr pqe' =>  101000,
+        '8 yr pqe' =>  103000,
+        'junior partner' =>  180000,
+        'Equity partner' =>  260000
+      }
     }
 
-    def initialize(student)
+    PLANS = {
+      'plan_2' => [{low: 0, high: 21000, value: 0},
+                   {low: 21000, high: 25000, value: 30},
+                   {low: 35000, high: 30000, value: 67},
+                   {low: 30000, high: 50000, value: 217},
+                   {low: 50000, high: 500000, value: 217}]
+    }
+
+    def salary_for(job_role, exp_level)
+      job_role = JOB_ROLES[job_role]
+      job_role.nil? ? nil : job_role[exp_level]
+    end
+
+    def repayment_by_income_and_plan(income, plan)
+      PLANS[plan].select{|i_to_r| income.between?(i_to_r[:low], i_to_r[:high])}.first
+    end
+  end
+
+  class Income
+    def initialize(student, db_interface)
       @student = student
+      @db_interface = db_interface
     end
 
     def value
-      EXP_LEVEL_TO_SALARY[@student.exp_level]
+      salary_for(@student.job_role, @student.exp_level)
     end
+
+    def salary_for(job_role, exp_level)
+      @db_interface.salary_for(job_role, exp_level)
+    end
+  end
+
+  class Repayment
+    def initialize(income, db_interface)
+      @income = income
+      @db_interface = db_interface
+    end
+
+    def value
+      repayment = repayment_for(@income.value)
+      repayment.nil? ? 0 : repayment[:value]
+    end
+
+    def repayment_for(income_value)
+      @db_interface.repayment_by_income_and_plan(income_value, 'plan_2')
+    end
+  end
+
+  class Interest
+
   end
 end
--- test/back_bullion_test.rb
+++ test/back_bullion_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class BackBullionTest < Minitest::Test
+  def setup
+    @db_interface = BackBullion::DBInterface.new
+  end
 
   def test_student
     student_params = {age: 35, job_role: 'laywer', tuition_fee: 9000, maintenance_loan: 3500, exp_level: '1st yr trainee'}
@@ -18,7 +21,7 @@ class BackBullionTest < Minitest::Test
     student_params = {age: 35, job_role: 'laywer', tuition_fee: 9000, maintenance_loan: 3500, exp_level: '1st yr trainee'}
     student = BackBullion::Student.new(student_params)
 
-    income = BackBullion::Income.new(student)
+    income = BackBullion::Income.new(student, @db_interface)
 
     assert_equal income.value, 38000
   end
@@ -27,12 +30,39 @@ class BackBullionTest < Minitest::Test
     student_params = {age: 35, job_role: 'laywer', tuition_fee: 9000, maintenance_loan: 3500, exp_level: '4 yr pqe'}
     student = BackBullion::Student.new(student_params)
 
-    income = BackBullion::Income.new(student)
+    income = BackBullion::Income.new(student, @db_interface)
 
     assert_equal income.value, 88000
   end
 
-  def test_repayment
+  def test_repayment_for_until_0_income
+    student_params = {age: 35, job_role: 'laywer', tuition_fee: 9000, maintenance_loan: 3500, exp_level: 'student'}
+    student = BackBullion::Student.new(student_params)
+    income = BackBullion::Income.new(student, @db_interface)
 
+    repayment = BackBullion::Repayment.new(income, @db_interface)
+
+    assert_equal repayment.value, 0
+  end
+
+  def test_repayment_for_until_88000_income
+    student_params = {age: 35, job_role: 'laywer', tuition_fee: 9000, maintenance_loan: 3500, exp_level: '4 yr pqe'}
+    student = BackBullion::Student.new(student_params)
+    income = BackBullion::Income.new(student, @db_interface)
+
+    repayment = BackBullion::Repayment.new(income, @db_interface)
+
+    assert_equal repayment.value, 217
+  end
+
+  def test_interest_for_student
+    skip
+    student_params = {age: 35, job_role: 'laywer', tuition_fee: 9000, maintenance_loan: 3500, exp_level: 'student'}
+    student = BackBullion::Student.new(student_params)
+    income = BackBullion::Income.new(student, @db_interface)
+
+    interest = BackBullion::Interest.new(income)
+
+    assert_equal interest.value, 0.053
   end
 end
